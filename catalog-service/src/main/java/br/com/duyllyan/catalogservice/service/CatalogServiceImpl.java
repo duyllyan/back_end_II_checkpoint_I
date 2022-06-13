849454643d39ce3f3eace213056a8ebb24855adc
Duyllyan <duyllyan@gmail.com>
2022-06-13 14:08:43 -0300

    Add gateway

--- catalog-service/src/main/java/br/com/duyllyan/catalogservice/service/CatalogServiceImpl.java
+++ catalog-service/src/main/java/br/com/duyllyan/catalogservice/service/CatalogServiceImpl.java
@@ -1,7 +1,9 @@
 package br.com.duyllyan.catalogservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CatalogServiceImpl implements CatalogService {
 
     @Autowired
--- catalog-service/src/main/java/br/com/duyllyan/catalogservice/service/MovieFeign.java
+++ catalog-service/src/main/java/br/com/duyllyan/catalogservice/service/MovieFeign.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "MOVIE")
+@FeignClient(value = "MOVIE-SERVICE")
 public interface MovieFeign {
     @RequestMapping(method = RequestMethod.GET, value = "/movie/{genre}")
     String findMovie(@PathVariable String genre);
